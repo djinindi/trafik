85f2ae3c3014a6f85535a3901d6c6791a03fb766
Jonas Jacobsen <jonasjacobsen@Fatherboat.local>
2015-10-26 12:42:54 +0100

    Transforming into 'Opholdstælling'

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope) {})
+.controller('LoginCtrl', function($scope) {
+  $scope.user = {
+    name: '',
+    street: '',
+    streetnr: '',
+    date: '',
+    time: '',
+  };
+})
 
-.controller('CountCtrl', function($scope, Vehicles) {
+.controller('CountCtrl', function($scope, categories, $cordovaDatePicker) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,19 +18,42 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $scope.location = "Lygten 16";
+  $scope.username = "Hans Jensen";
+  $scope.categories = categories.all();
+  $scope.remove = function(category) {
+    if (category.count >= 1) {
+      category.count--;
+    }
+  };
+  $scope.plus = function(category) {
+    category.count++;
+  };
 
-  $scope.vehicles = Vehicles.all();
-  $scope.remove = function(vehicle) {
-    Vehicles.remove(vehicle);
+  $scope.time = function() {
+    var options = {
+      date: new Date(),
+      mode: 'time',
+      doneButtonLabel: 'DONE',
+      doneButtonColor: '#F2F3F4',
+      cancelButtonLabel: 'CANCEL',
+      cancelButtonColor: '#000000',
+      minuteInterval: 15
+    };
+    $cordovaDatePicker.show(options).then(function(date) {
+      alert(date);
+    }, false);
   };
 })
 
-.controller('VehicleDetailCtrl', function($scope, $stateParams, Vehicles) {
-  $scope.vehicle = Vehicles.get($stateParams.vehicleId);
+.controller('VehicleDetailCtrl', function($scope, $stateParams, categories) {
+  $scope.category = categories.get($stateParams.vehicleId);
 })
 
 .controller('SettingsCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
   };
+  function uploadData() {
+
+  }
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,35 +1,76 @@
 angular.module('starter.services', [])
 
-.factory('Vehicles', function() {
+.factory('categories', function() {
   // Might use a resource here that returns a JSON array
-  var vehicles = [{
+  var categories = [{
     id: 0,
-    name: 'Personbil',
-    image: '/img/Car-Side-View-128.png',
-    count: 0
+    name: 'Stående børn',
+    count: 34
   }, {
     id: 1,
-    name: 'Cykel',
-    image: '/img/Cycle-Race-128.png',
+    name: 'Stående voksne',
     count: 12
   }, {
     id: 2,
-    name: 'Bus',
-    image: '/img/Bus-Side-View-128.png',
+    name: 'Ventende på bus',
     count: 2
+  }, {
+    id: 3,
+    name: 'Siddende på café',
+    count: 1
+  }, {
+    id: 4,
+    name: 'Siddende på bænk - børn',
+    count: 2
+  }, {
+    id: 5,
+    name: 'Siddende på bænk - voksne',
+    count: 42
+  }, {
+    id: 6,
+    name: 'Siddende andre steder - børn',
+    count: 14
+  }, {
+    id: 7,
+    name: 'Siddende andre steder - voksne',
+    count: 3
+  }, {
+    id: 8,
+    name: 'Barnevogne',
+    count: 0
+  }, {
+    id: 9,
+    name: 'Liggende',
+    count: 0
+  }, {
+    id: 10,
+    name: 'Legende børn',
+    count: 1
+  }, {
+    id: 11,
+    name: 'Legende / trænende voksne',
+    count: 0
+  }, {
+    id: 12,
+    name: 'Kommercielle aktiviteter',
+    count: 3
+  }, {
+    id: 13,
+    name: 'Kultur aktiviteter',
+    count: 0
   }];
 
   return {
     all: function() {
-      return vehicles;
+      return categories;
     },
-    remove: function(vehicle) {
-      vehicles.splice(vehicles.indexOf(vehicle), 1);
+    remove: function(category) {
+      categories.splice(categories.indexOf(category), 1);
     },
-    get: function(vehicleId) {
-      for (var i = 0; i < vehicles.length; i++) {
-        if (vehicles[i].id === parseInt(vehicleId)) {
-          return vehicles[i];
+    get: function(categoryId) {
+      for (var i = 0; i < categories.length; i++) {
+        if (categories[i].id === parseInt(categoryId)) {
+          return categories[i];
         }
       }
       return null;
